a59fccaf17c5233bcf6f314357e7231d333f9ced
Vinayak <vinayak.gupta@codexlab.in>
2026-08-25 11:47:51 +0530

    updated the new schema and validation and remove the media validation

--- src/validations/media.validation.ts
+++ src/validations/media.validation.ts
@@ -1,36 +1,36 @@
-import { z } from "zod";
+// import { z } from "zod";
 
-// Base Reusable Schemas
-import { objectIdSchema } from "./common.validation.js";
-export const mediaOrientationEnum = z.enum(["portrait", "landscape", "unknown"]);
+// // Base Reusable Schemas
+// import { objectIdSchema } from "./common.validation.js";
+// export const mediaOrientationEnum = z.enum(["portrait", "landscape", "unknown"]);
 
-// Generic Media Builder
-const createMediaSchema = (urlErrorMsg: string) =>
-  z.object({
-    label: z.string().optional(),
-    url: z.string().trim().min(1, urlErrorMsg),
-    orientation: mediaOrientationEnum.optional(),
-  });
+// // Generic Media Builder
+// const createMediaSchema = (urlErrorMsg: string) =>
+//   z.object({
+//     label: z.string().optional(),
+//     url: z.string().trim().min(1, urlErrorMsg),
+//     orientation: mediaOrientationEnum.optional(),
+//   });
 
-export const imageItemSchema = createMediaSchema("Image URL is required");
-export const videoItemSchema = createMediaSchema("Video URL is required");
+// export const imageItemSchema = createMediaSchema("Image URL is required");
+// export const videoItemSchema = createMediaSchema("Video URL is required");
 
-// Grouped Media Schemas
-const imageList = z.array(imageItemSchema).optional();
+// // Grouped Media Schemas
+// const imageList = z.array(imageItemSchema).optional();
 
-export const imagesSchema = z.object({
-  apartment: imageList,
-  ext_view_day: imageList,
-  ext_view_night: imageList,
-  amenities: imageList,
-  plans: imageList,
-});
+// export const imagesSchema = z.object({
+//   apartment: imageList,
+//   ext_view_day: imageList,
+//   ext_view_night: imageList,
+//   amenities: imageList,
+//   plans: imageList,
+// });
 
-export const listingMediaValidationSchema = z.object({
-  listing_id: objectIdSchema,
-  images: imagesSchema.optional(),
-  videos: z.array(videoItemSchema).optional(),
-});
+// export const listingMediaValidationSchema = z.object({
+//   listing_id: objectIdSchema,
+//   images: imagesSchema.optional(),
+//   videos: z.array(videoItemSchema).optional(),
+// });
 
-// Types
-export type ListingMediaInput = z.infer<typeof listingMediaValidationSchema>;
+// // Types
+// export type ListingMediaInput = z.infer<typeof listingMediaValidationSchema>;
